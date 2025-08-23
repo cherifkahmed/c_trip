f817e322ea460bcd9fde52e01e3c0bdc22d2e345
cherifkahmed <cherif.k.ahmed@gmail.com>
2025-08-23 13:18:28 +0000

    that shit finaly work

--- Frombooks/arrays/ex5/random_walk.c
+++ Frombooks/arrays/ex5/random_walk.c
@@ -1,65 +1,71 @@
-// #include <stdio.h>
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
-
 int main(void) {
-    char board[10][10], letter = 'A';
-    int i, j;
-    int random_direction = 1;
-    bool stuck = false;
-    bool out = false;
+    int i,j,old_i,old_j;
+    char board[10][10],next_char = 'A';
+    int r_num = rand() % 4;
+
+    //filling the table
+
     for (i = 0; i < 10; i++) {
+
         for (j = 0; j < 10; j++) {
             board[i][j] = '.';
         }
     }
+
+    // Doing the pikaboo stuf
+    srand(time(NULL));
     i = 0;
     j = 0;
-
-    while (letter < 'Z' && !stuck) {
-        srand(time(NULL));
-        random_direction = rand() % 4;
-        switch (random_direction) {
-            case 0: j--; break;
-            case 1: i--; break;
-            case 2: j++; break;
-            case 3: i++; break;
-            default: printf("not a valid direction"); break;
+    old_i = i;
+    old_j = j;
+    board[i][j] = next_char;
+    bool stuck = false;
+    while (next_char < 'Z') {
+        r_num = rand() % 4;
+        switch (r_num) {
+            case 0: j--; printf("j = %d\n",j); break;
+            case 1: i++; printf("i = %d\n",i); break;
+            case 2: j++; printf("j = %d\n",j); break;
+            case 3: i--; printf("i = %d\n",i); break;
+            default: printf("out of range");
         }
-        if (i < 0 || i > 10 || j < 0 || j > 10) {
-            out = true;
-        }
-        while (out) {
-            srand(time(NULL));
-            random_direction = rand() % 4;
-            switch (random_direction) {
-                case 0: j--;printf("j = %d",j); break;
-                case 1: i--; printf("i = %d",i);break;
-                case 2: j++; printf("j = %d",j);break;
-                case 3: i++; printf("i = %d",j);break;
-                default: printf("not a valid direction"); break;
+        if (i >= 0 && j >= 0 && i < 10 && j < 10 && board[i][j] == '.') {
+            if (board[i - 1][j] != '.' &&
+                board[i + 1][j] != '.' &&
+                board[i][j - 1] != '.' &&
+                board[i][j + 1] != '.') {
+                goto display;
             }
-            if (!(i < 0 || i > 10 || j < 0 || j > 10)) {
-                out = false;
-            }
+            next_char++;
+            board[i][j] = next_char;
+            old_i = i;
+            old_j = j;
+        }else {
+            i = old_i;
+            j = old_j;
         }
-        if (board[i][j] != '.') {
-            printf("Fix that shit man");
-            continue;
-        }
-        board[i][j] = letter;
-        letter++;
 
     }
 
 
 
+
+
+    // displaying the table 
+display : 
+    printf("\n");
+    if (next_char != 'Z') {
+        board[i][j] = next_char + 1;
+    }
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
-            printf("%c ", board[i][j]);
+            printf("%c ",board[i][j]);
         }
         printf("\n");
     }
